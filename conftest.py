b5b25bf98bff70e77fd4a58266b0c60020326757
Vitaly <vitalik.borisov.1997@mail.ru>
2026-04-04 04:57:44 +0500

    ну как попробую без options

--- conftest.py
+++ conftest.py
@@ -4,8 +4,9 @@ from browser.browser_factory import BrowserFactory
 
 @pytest.fixture
 def browser():
-    docker_options = ["--headless", "--no-sandbox", "--disable-dev-shm-usage"]
-    driver = BrowserFactory.get_driver(options=docker_options)
+    #docker_options = ["--headless", "--no-sandbox", "--disable-dev-shm-usage"]
+    # driver = BrowserFactory.get_driver(options=docker_options)
+    driver = BrowserFactory.get_driver()
     browser = Browser(driver)
     yield browser
     browser.quit()
