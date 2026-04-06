c29421d1cc8fe6e5cc11c8a66d42ab27ce155dc2
Vitaly <vitalik.borisov.1997@mail.ru>
2026-04-07 03:11:51 +0500

    настроил разрешение экрана чтобы автотест не падал

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ from browser.browser_factory import BrowserFactory
 
 @pytest.fixture
 def browser():
-    docker_options = ["--headless", "--no-sandbox", "--disable-dev-shm-usage"]
+    docker_options = ["--headless", "--no-sandbox", "--disable-dev-shm-usage", "--window-size=1920,1080"]
     driver = BrowserFactory.get_driver(options=docker_options)
     browser = Browser(driver)
     yield browser
